7c1f4f0359fd04a42f3595168845d31d9dd57c82
Sadfroggy0 <tim.panov1337@gmail.com>
2025-05-08 20:36:06 +0300

    local usage fix

--- src/main/java/timofey/ApplicationRunner.java
+++ src/main/java/timofey/ApplicationRunner.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.PropertySource;
 import java.util.List;
 
 @SpringBootApplication
-@PropertySource("classpath:values.properties")
+@PropertySource("classpath:config/values.properties")
 public class ApplicationRunner {
     public static void main(String[] args) {
         SpringApplication.run(ApplicationRunner.class, args);
